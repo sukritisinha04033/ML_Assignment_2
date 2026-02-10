42007cc97e69b8472815f1652a4a5b11ef00363a
sukritisinha04033 <2024dc04033@wilp.bits-pilani.ac.in>
2026-02-11 00:13:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import joblib
+import json
 import pandas as pd
 
 st.title("Breast Cancer Classifier")
@@ -39,3 +40,6 @@ file = st.file_uploader("Upload CSV")
 if file:
     data = pd.read_csv(file)
     st.write(data.head())
+
+with open("metrics.json") as f:
+    metrics_data = json.load(f)
